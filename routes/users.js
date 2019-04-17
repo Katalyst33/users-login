94e33a8453f5d93b0992bc19e7fd8c089c05debe
xam <snmje@gmail.com>
2019-04-17 16:27:47 +0100

    added user models

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ let fs = require('fs');
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//user model
+const User = require('../models/User');
 
 
 // Load Chance
@@ -17,45 +21,83 @@ router.get('/login', (req, res) => res.render('./Login'));
 
 
 //Login
-router.get('/register', (req, res) =>  res.render('Register'));
+router.get('/register', (req, res) => res.render('Register'));
 
 //register handler
 router.post('/register', (req, res) => {
 
 
-    const { name, email, password, password2} = req.body;
+    const {name, email, password, password2} = req.body;
 
     let errors = [];
 
     //Check required fields
-    if(!name || !email || !password || !password2){
+    if (!name || !email || !password || !password2) {
         errors.push({msg: 'Please fill in all fields'});
     }
 
     // Check passwords match
-    if(password !== password2) {
-        errors.push({ msg: 'Passwords do not match'});
+    if (password !== password2) {
+        errors.push({msg: 'Passwords do not match'});
     }
 
     //check password length
-    if(password.length < 6) {
-        errors.push({msg: 'password should be a5 least 6 characters'});
+    if (password.length < 6) {
+        errors.push({msg: 'password should be at least 6 characters'});
     }
 
-    if (errors.length> 0) {
-    res.render('register',{
-        errors,
-        name,
-        email,
-        password,
-        password2
-    });
-    }else {
-        res.send('pass');
-    }
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        //Validation passed
+        User.findOne({email: email})
+            .then(user => {
+                if (user) {
+                    //User exist
 
-});
+                    errors.push({msg: 'Email is already registered'});
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    // print form detail to console
+                    /*  console.log(newUser);
+                      res.send('hello');*/
+
+                    //hash password
+                    bcrypt.genSalt(10, (err, salt) =>
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            //set password to hased
+                            newUser.password = hash;
+                            //save user
+                            newUser.save()
+                                .then(user => {
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => console.log(err));
+
+                        }))
+
+                }
 
 
+                module.exports = router;
 
-module.exports = router;
+
