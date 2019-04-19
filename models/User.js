c305aca9ff0161d3d7534fd1b2d9909970f103bb
xam <snmje@gmail.com>
2019-04-19 05:29:25 +0100

    fixed big bug error

--- models/User.js
+++ models/User.js
@@ -1,26 +1,22 @@
 const mongoose = require('mongoose');
 
-const  UserSchema =  new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
-
     },
-    email:{
-        type:String,
-        required:true
-    },
-
-    password:{
-        type:String,
+    email: {
+        type: String,
         required: true
     },
-    date:{
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
         type: Date,
         default: Date.now
-    },
-
-
+    }
 });
 
 const User = mongoose.model('User', UserSchema);
